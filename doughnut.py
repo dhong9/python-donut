e3f527f907c04ee46693029fa796ce5d5c9c1447
Daniel Hong <dhong9@users.noreply.github.com>
2019-10-01 22:43:55 -0700

    Changed how icing done
    I changed it to a fluid simulator where the doughnut is the obstacle, outer cube is the domain, and the icing is the fluid.

--- doughnut.py
+++ doughnut.py
@@ -13,15 +13,16 @@ def doughnut():
         bpy.ops.mesh.select_all(action = 'DESELECT')
     
     # Units are in meters
-    radius = uniform(0.025, 0.05)
+    r = uniform(0.025, 0.05)
+    mr = r / uniform(1.8, 3)
     
     # Add torus
     bpy.ops.mesh.primitive_torus_add(
         align = 'WORLD', 
         location = (0, 0, 0),
         rotation = (0, 0, 0), 
-        major_radius = radius, 
-        minor_radius = radius / uniform(1.8, 3),
+        major_radius = r, 
+        minor_radius = mr,
         major_segments = 28,
         minor_segments = 16,
         abso_major_rad = 1.25, 
@@ -86,43 +87,31 @@ def doughnut():
     # Add subsurf modifer
     bpy.ops.object.modifier_add(type='SUBSURF')
     
-    # Go back to edit mode
-    bpy.ops.object.mode_set(mode = 'EDIT')
+    # Make the doughnut a fluid obstacle
+    bpy.ops.object.modifier_add(type='FLUID_SIMULATION')
+    bpy.context.object.modifiers["Fluidsim"].settings.type = 'OBSTACLE'
     
-    # Select the top half
-    for i in top_doughnut:
-        bpy.ops.object.mode_set(mode = 'OBJECT')
-        obj = bpy.context.active_object
-        bpy.ops.object.mode_set(mode = 'EDIT') 
-        bpy.ops.mesh.select_mode(type = "VERT")
-        bpy.ops.object.mode_set(mode = 'OBJECT')
-        obj.data.vertices[i].select = True
-        bpy.ops.object.mode_set(mode = 'EDIT')
-        
-    # Duplicate it
-    bpy.ops.mesh.duplicate_move(
-        MESH_OT_duplicate = {"mode" : 1}, 
+    # Add a circle
+    icingRadius = uniform(r - mr, r)
+    bpy.ops.mesh.primitive_circle_add(radius = icingRadius, enter_editmode = True, location = (0, 0, 1.55 * mr))
+    bpy.ops.mesh.extrude_region_move(
+        MESH_OT_extrude_region = {
+            "use_normal_flip": False, 
+            "mirror": False
+        }, 
         TRANSFORM_OT_translate = {
             "value": (0, 0, 0), 
             "orient_type": 'GLOBAL', 
-            "orient_matrix":(
-                (0, 0, 0), 
-                (0, 0, 0), 
-                (0, 0, 0)
-            ), 
+            "orient_matrix": ((0, 0, 0), (0, 0, 0), (0, 0, 0)), 
             "orient_matrix_type": 'GLOBAL', 
             "constraint_axis": (False, False, False), 
             "mirror": False, 
             "use_proportional_edit": False, 
-            "proportional_edit_falloff": 'SMOOTH', 
-            "proportional_size": 1, 
-            "use_proportional_connected": False, 
-            "use_proportional_projected": False, 
             "snap": False, 
-            "snap_target":'CLOSEST', 
-            "snap_point":(0, 0, 0), 
+            "snap_target": 'CLOSEST', 
+            "snap_point": (0, 0, 0), 
             "snap_align": False, 
-            "snap_normal":(0, 0, 0), 
+            "snap_normal": (0, 0, 0), 
             "gpencil_strokes": False, 
             "cursor_transform": False, 
             "texture_space": False, 
@@ -132,16 +121,77 @@ def doughnut():
         }
     )
     
-    # Separate it
-    # It's going to be icing
-    bpy.ops.mesh.separate(type = 'SELECTED')
-    bpy.ops.object.mode_set(mode = 'OBJECT')
-    bpy.context.view_layer.objects.active = bpy.context.selected_objects[1]
+    # Scale up the icing
+    icingScale = uniform(1.25, 1.5) # Will need to tweak these around
+    bpy.ops.transform.resize(
+        value = (icingScale * r / icingRadius, icingScale * r / icingRadius, icingScale * r / icingRadius),
+        orient_type = 'GLOBAL', 
+        orient_matrix = ((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+        orient_matrix_type = 'GLOBAL', 
+        mirror = True, 
+        use_proportional_edit = False,
+        proportional_size = 1, 
+        use_proportional_connected = False, 
+        use_proportional_projected = False
+    )
     
-    # Add thickness to the icing
-    bpy.ops.object.modifier_add(type='SOLIDIFY')
-    bpy.context.object.modifiers["Solidify"].offset = 1
-    bpy.context.object.modifiers["Solidify"].thickness = 0.0025 
-    bpy.ops.object.modifier_move_up(modifier="Solidify") # Make sure that the solidify modifer is applied before subsurf
+    # Extrude it up
+    bpy.ops.mesh.select_all(action = 'SELECT')
+    bpy.ops.mesh.extrude_region_move(
+        MESH_OT_extrude_region = {
+            "use_normal_flip" : False, 
+            "mirror" : False
+        }, 
+        TRANSFORM_OT_translate = {
+            "value" : (0, 0, 0.0025), 
+            "orient_type": 'LOCAL', 
+            "orient_matrix": ((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+            "orient_matrix_type": 'LOCAL', 
+            "constraint_axis": (False, False, True), 
+            "mirror": False, 
+            "use_proportional_edit": False, 
+            "proportional_edit_falloff":'SMOOTH', 
+            "snap": False, 
+            "snap_target": 'CLOSEST', 
+            "snap_point": (0, 0, 0), 
+            "snap_align": False, 
+            "snap_normal": (0, 0, 0), 
+            "gpencil_strokes": False, 
+            "cursor_transform": False, 
+            "texture_space": False, 
+            "remove_on_cancel": False, 
+            "release_confirm": False, 
+            "use_accurate": False
+        }
+    )
+    
+    # Recalculate normals
+    bpy.ops.mesh.select_all(action = 'SELECT')
+    bpy.ops.mesh.normals_make_consistent(inside = False)
+    
+    bpy.ops.object.mode_set(mode = 'OBJECT')  
+    
+    # Make that icing a fluid
+    bpy.ops.object.modifier_add(type='FLUID_SIMULATION')
+    bpy.context.object.modifiers["Fluidsim"].settings.type = 'FLUID'
+    
+    # Add a box
+    bpy.ops.mesh.primitive_cube_add(size = 2, enter_editmode = False, location = (0, 0, 0))
+    bpy.ops.transform.resize(
+        value = (r * 2, r * 2, r), 
+        orient_type= 'GLOBAL', 
+        orient_matrix = ((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+        orient_matrix_type = 'GLOBAL', 
+        mirror = True, 
+        use_proportional_edit = False
+    )
+    
+    # Maie that a fluid domain
+    bpy.ops.object.modifier_add(type='FLUID_SIMULATION')
+    bpy.context.object.modifiers["Fluidsim"].settings.type = 'DOMAIN'
+    bpy.context.object.modifiers["Fluidsim"].settings.resolution = 90
+    bpy.context.object.modifiers["Fluidsim"].settings.viscosity_base = 1
+    bpy.context.object.modifiers["Fluidsim"].settings.viscosity_exponent = 2
 
+    
 doughnut()
